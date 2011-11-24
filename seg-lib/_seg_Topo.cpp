9874136abbc9db4aa2b3371881a0101073ddab35
Jorge Cardoso <manuel.cardoso@ucl.ac.uk>
2011-11-24 16:50:00 +0000

    bugfix

--- seg-lib/_seg_Topo.cpp
+++ seg-lib/_seg_Topo.cpp
@@ -152,6 +152,10 @@ void Dillate(bool * Image,
     shiftdirection[0]=1;
     shiftdirection[1]=dimensions[0];
     shiftdirection[2]=dimensions[1]*dimensions[0];
+    if(verbose>1){
+        cout<<"Buffer allocating size:" << dimensions[1]*dimensions[0]*dimensions[2] << " - " << dimensions[0]<<" - " << dimensions[1]<<" - " << dimensions[2]<<endl;
+        flush(cout);
+    }
     bool * Buffer = new bool [dimensions[1]*dimensions[0]*dimensions[2]]();
     if(verbose>1){
         cout<<"Buffer allocated sucessfully"<<endl;
